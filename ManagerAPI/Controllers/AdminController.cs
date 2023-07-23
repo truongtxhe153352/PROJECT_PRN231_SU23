b91c8ba6ee420d3794413baf2cc278ae21d17a0b
truongtxhe153352 <truongtxhe153352@fpt.edu.vn>
2023-07-23 11:53:59 +0700

    ADD ODATA AND REFERENCE

---
+++ ManagerAPI/Controllers/AdminController.cs
@@ -0,0 +1,89 @@
+﻿using AutoMapper;
+using BusinessObjects.DTO;
+using BusinessObjects.Models;
+using Microsoft.AspNetCore.Mvc;
+using Repositories.Interface;
+using Repositories;
+
+namespace ManagerAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class AdminController : Controller
+    {
+        private readonly ICourseRepository _courseRepository = new CourseRepository();
+        private readonly IMapper _mapper;
+
+        public AdminController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public IActionResult GetAllCourse()
+        {
+            List<Course> courses = _courseRepository.GetAllCourse();
+            List<CourseDto> courseDtos = new List<CourseDto>();
+            foreach (var course in courses)
+            {
+                courseDtos.Add(_mapper.Map<Course, CourseDto>(course));
+            }
+            return Ok(courseDtos);
+        }
+
+        [HttpGet("{courseId}")]
+        public IActionResult GetCourseById(int courseId)
+        {
+            Course course = _courseRepository.GetCourseById(courseId);
+            CourseDto courseDto = _mapper.Map<Course, CourseDto>(course);
+            return Ok(courseDto);
+        }
+        [HttpPost]
+        public IActionResult InsertCourse(Course course)
+        {
+            try
+            {
+                _courseRepository.InsertCourse(course);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateCourse(Course course)
+        {
+            try
+            {
+                _courseRepository.UpdateCourse(course);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return Ok();
+        }
+        [HttpDelete("{courseId}")]
+        public IActionResult DeleteCourse(int courseId)
+        {
+            var c = _courseRepository.GetCourseById(courseId);
+            if (c == null)
+            {
+                return NotFound();
+            }
+            var materialCourse = c.Materials.Where(a => a.CourseId == courseId).ToList();
+            List<string> mPaths = c.Materials.Select(m => m.Path + "/" + m.MaterialName).ToList();
+            foreach (var m in mPaths)
+            {
+                if (System.IO.File.Exists(m))
+                {
+                    System.IO.File.Delete(m);
+                }
+            }
+            _courseRepository.DeleteCourse(c);
+
+            return Ok();
+        }
+    }
+}
--- ManagerAPI/Program.cs
+++ ManagerAPI/Program.cs
@@ -1,3 +1,35 @@
+//var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container.
+
+//builder.Services.AddControllers();
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.Run();
+
+
+using AutoMapper;
+using BusinessObjects.DTO;
+using BusinessObjects.Models;
+using Microsoft.AspNetCore.OData;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +38,24 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// oder data
+builder.Services.AddControllers().AddOData(option => option.Select().Filter().Count().OrderBy().Expand());
+
+
+// add configure 
+builder.Services.AddCors();
+builder.Services.AddDbContext<PRN231_ProjectContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("MyDatabase"));
+});
+var mapperConfig = new MapperConfiguration(mc =>
+{
+    mc.AddProfile(new MyMapperProfile());
+});
+IMapper mapper = mapperConfig.CreateMapper();
+builder.Services.AddSingleton(mapper);
+
 
 var app = builder.Build();
 
